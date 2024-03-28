dd49f6b242348854e2afad4d558e92fbe8942d0d
0xHy0kR1 <helloweenhell666@gmail.com>
2024-03-28 15:43:42 +0530

    few changes to parse 'headers', 'method', 'path', 'body'

--- parsed_logs.py
+++ parsed_logs.py
@@ -1,10 +1,10 @@
-import http.client
 import xml.etree.ElementTree as ET
 from burpy.rawweb import RawWeb
 import urllib.parse as urllib
-
+import base64
+import csv
 log_path = 'burp.log'
-
+output_log = 'httplog.csv'
 def parse_log(log_path):
     '''
     This function accepts burp log file path.
@@ -31,4 +31,52 @@ def parse_log(log_path):
         result[raw_req] = raw_resp
     return result
 
-print(parse_log(log_path))
+def parseRawHTTPReq(rawReq):
+    try:
+        raw = rawReq.decode('utf8')
+    except Exception as e:
+        raw = rawReq
+    global headers,method,body,path
+    headers = {}
+    sp = raw.split('\n\n', 1)
+    if len(sp) > 1:
+        head = sp[0]
+        body = sp[1]
+    else:
+        head = sp[0]
+        body = ""
+    c1 = head.split('\n', head.count('\n'))
+    method = c1[0].split(' ', 2)[0]
+    if len(c1[0].split(' ', 2)) >= 2:
+        path = c1[0].split(' ', 2)[1]
+    else:
+        path = ""
+    for i in range(1, head.count('\n')+1):
+        slice1 = c1[i].split(': ', 1)
+        if slice1[0] != "":
+            headers[slice1[0]] = slice1[1]
+    return headers, method, body, path
+
+# Open the log file
+with open(output_log, 'w') as csvfile:
+    c = csv.writer(csvfile)
+    c.writerow(["method", "body", "path", "headers"])
+
+result = parse_log(log_path)
+for items in result:
+    data = []
+    try:
+        decoded_request = base64.b64decode(items).decode('utf-8')  # Try decoding as UTF-8
+        lines = decoded_request.split('\r\n')  # Split the request into lines
+        parsed_request = '\n'.join(lines)  # Join the lines into a single string
+        headers, method, body, path = parseRawHTTPReq(parsed_request)  # Pass the single string to the function
+        data.append(method)
+        data.append(body)
+        data.append(path)
+        data.append(headers)
+        with open(output_log, 'a') as csvfile:
+            c = csv.writer(csvfile)
+            c.writerow(data)
+
+    except UnicodeDecodeError:
+        print("[+] Error decoding request data: UnicodeDecodeError")
